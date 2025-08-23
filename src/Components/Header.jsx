e83758a00ca01add57d7dd46373bff1d98f3120e
Salohiddin2012 <salohiddinehsonov@gmail.com>
2025-08-23 10:41:53 +0500

    react-router intall

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,21 @@
-
+import { use } from 'react';
+import logo from '../assets/frame.png';
+import { useNavigate } from 'react-router';
 
 function Header() {
+    const navigate = useNavigate();
     return (
     <div>
-        
+        <header>
+            <div className="container mx-auto px-4">
+                <img src={logo} alt="" />
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Contact</a>
+                </nav>
+            </div>
+        </header>
     </div>
     );
 }
